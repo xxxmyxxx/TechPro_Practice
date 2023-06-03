4b8790aeb939bbc33125b95d4b388de0ad5296ab
Mustafa <yazilimcimiyim@gmail.com>
2023-06-03 15:53:27 +0300

    Arrays

---
+++ src/main/java/day_05/C05_DoWhileLoop.java
@@ -0,0 +1,10 @@
+package day_05;
+
+public class asd {
+       /*
+    Bir top belirli yükseklikten atılmaktadır.(Scanner ile kullanıcıdan yüksekliği alınız)
+    Atıldıktan sonra, atıldığı yüksekliğin 1/2 si kadar yerden yukarı doğru zıplamaktadır
+    Top zıplama  yüksekliği 1 metrenin altına indiğinde durmaktadır.
+    Bu ana kadar aldigi TOPLAM YOLU ve YERE VURMA SAYISINI bulan do while code blogu create ediniz.
+    */
+}
